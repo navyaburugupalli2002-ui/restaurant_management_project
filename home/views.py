d493dcd8b6f87d8ec577456d5bf95268244dcc7b
navyaburugupalli2002-ui <navyaburugupalli2002@gmail.com>
2025-08-11 14:45:52 +0530

    23366

--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ def home_view(request):
     restaurant=Restaurant.objects.first()
     restaurant_name=restaurant.name
 if restaurant else "Restaurant Name Not Set"
-    return render(request, 'home/index.html', {'restaurant_name': restaurant_name})
+    return render(request, 'home/index.html', {'restaurant_name': Full Moon})
