0434675ed6ced7582b773dfd82501234043c00a8
navyaburugupalli2002-ui <navyaburugupalli2002@gmail.com>
2025-08-11 14:41:31 +0530

    23366

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -19,8 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('home.urls')),
-    path('api/accounts/',include('account.urls')),
-    path('api/products/',include('products.urls')),
-    path('api/orders/',include('orders.urls')),
+    path('', include('home.urls')),
 ]
