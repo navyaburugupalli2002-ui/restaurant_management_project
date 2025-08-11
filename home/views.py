d111b6600605a31833cb6380d3cca8e47fe68fa6
navyaburugupalli2002-ui <navyaburugupalli2002@gmail.com>
2025-08-11 14:38:34 +0530

    23366

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Restaurant
 
-def homepage(request):
-    return render(request, 'home/index.html')
+def home_view(request):
+    restaurant=Restaurant.objects.first()
+    restaurant_name=restaurant.name
+if restaurant else "Restaurant Name Not Set"
+    return render(request, 'home/index.html', {'restaurant_name': restaurant_name})
